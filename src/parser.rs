52895c96dc7c37e91f0483e5f416601eecddccb4
Shanavas M <shanvas@buroojsystems.com>
2017-08-21 10:11:34 +0300

    Implement Iterator, Peekable for parser
    peekable is implemented by copying method directly

--- src/parser.rs
+++ src/parser.rs
@@ -6,57 +6,61 @@ use token::*;
 
 pub struct Parser<'a> {
     lexer: Lexer<'a>,
-    ast: Vec<Expr>,
+    peeked: Option<Option<Token>>,
+}
+
+impl<'a> Iterator for Parser<'a> {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Token> {
+        match self.peeked.take() {
+            Some(v) => v,
+            None => self.lexer.next(),
+        }
+    }
+
+}
+
+impl <'a> Parser<'a> {
+    pub fn peek(&mut self) -> Option<&Token> {
+        if self.peeked.is_none() {
+            self.peeked = Some(self.lexer.next());
+        }
+        match self.peeked {
+            Some(Some(ref value)) => Some(value),
+            Some(None) => None,
+            _ => unreachable!(),
+        }
+    }
 }
 
 impl<'a> Parser<'a> {
     pub fn new(lexer: Lexer) -> Parser {
         Parser {
             lexer: lexer,
-            ast: vec![],
+            peeked: None,
         }
     }
 
-    pub fn parse(mut self) -> Result<Vec<Expr>, String> {
-        while let Some(tk) = self.lexer.next() {
+    pub fn parse(mut self) -> Result<Expr, String> {
+        while let Some(tk) = self.next() {
             let expr = match tk.token_type() {
                 TokenType::Character(_) => self.parse_character(tk),
                 _ => Err(format!("error occured")),
             };
 
             if let Ok(v) = expr {
-                self.ast.push(v);
             }
         }
 
         if self.lexer.is_error() {
             Err(format!("error occured"))
         } else {
-            Ok(self.ast)
+            unimplemented!("")
         }
     }
 
     fn parse_character(&mut self, tk: Token) -> Result<Expr, String> {
-        match tk.token_type() {
-            TokenType::Character(Characters::Literally) |
-            TokenType::Character(Characters::OneOf) |
-            TokenType::Character(Characters::Raw) => {
-                if let Some(val) = self.lexer.next() {
-                    match val.token_type() {
-                        TokenType::String => Ok(Expr::from(ast::Character {
-                            ty: tk,
-                            spec: Some(ast::Specification::String(val.val())),
-                        })),
-                        _ => Err(format!("expected string, found {:?}", val)),
-                    }
-                } else {
-                    Err(format!("expected string"))
-                }
-            },
-            TokenType::Character(_) => {
-                Ok(Expr::from(tk))
-            },
-            _ => Err(format!("expected character, found {:?}", tk)),
-        }
+        unimplemented!("")
     }
 }
